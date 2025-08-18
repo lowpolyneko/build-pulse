fcc0c48c31d6e1c12e9806c8c4b9be628d155c48
Ethan Wong <ewong@anl.gov>
2025-08-18 14:10:18 -0500

    feat: basic artifact retrieval

--- src/db/mod.rs
+++ src/db/mod.rs
@@ -4,6 +4,7 @@ use std::ops::{Deref, DerefMut};
 
 use rusqlite::{Connection, Params, Result, Row};
 
+mod artifact;
 mod build;
 mod issue;
 mod job;
@@ -12,7 +13,7 @@ mod similarity;
 mod stats;
 mod tag;
 
-pub use {build::*, issue::*, job::*, run::*, similarity::*, stats::*, tag::*};
+pub use {artifact::*, build::*, issue::*, job::*, run::*, similarity::*, stats::*, tag::*};
 
 /// Read [serde] serialized value from `row` and `idx`
 #[macro_export]
@@ -155,7 +156,7 @@ macro_rules! for_all {
     };
 
     ($($method:tt)+) => {
-        for_all!([Job, JobBuild, Run, Issue, TagInfo, SimilarityInfo] => $($method)+)
+        for_all!([Artifact, Job, JobBuild, Run, Issue, TagInfo, SimilarityInfo] => $($method)+)
     };
 }
 
